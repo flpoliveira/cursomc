0297f9800f947a86a2ca1c2c26b1d7b457b61bc7
Filipe <filipedasilvadeoliveira@gmail.com>
2018-09-19 17:43:03 -0300

    ItemPedido e ItemPedidoPK - Parte 1

--- src/main/java/com/flpoliveira/cursomc/domain/Pedido.java
+++ src/main/java/com/flpoliveira/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.flpoliveira.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -34,6 +36,8 @@ public class Pedido  implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido()
 	{
@@ -87,6 +91,14 @@ public class Pedido  implements Serializable {
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -112,7 +124,8 @@ public class Pedido  implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 	  
 }
--- src/main/java/com/flpoliveira/cursomc/domain/Produto.java
+++ src/main/java/com/flpoliveira/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.flpoliveira.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -30,6 +32,8 @@ public class Produto implements Serializable
 	private String nome;
 	private Double preco;
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name= "PRODUTO_CATEGORIA",
@@ -49,6 +53,17 @@ public class Produto implements Serializable
 		this.nome = nome;
 		this.preco = preco;
 	}
+	
+	public List<Pedido> getPedidos()
+	{
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens)
+		{
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 
 	public Integer getId() {
 		return id;
@@ -81,6 +96,14 @@ public class Produto implements Serializable
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
